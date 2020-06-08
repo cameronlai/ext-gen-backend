3530410b97c3086e2421d432a00b7bbee4ecf108
Cameron Lai <cameronlai@users.noreply.github.com>
2020-06-08 21:21:40 +0800

    Enable CORS

--- main.py
+++ main.py
@@ -7,10 +7,9 @@ def main(request):
         Response object using
         `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
     """
-    request_json = request.get_json()
-    if request.args and 'message' in request.args:
-        return request.args.get('message')
-    elif request_json and 'message' in request_json:
-        return request_json['message']
-    else:
-        return f'Hello World!'
+    # Set CORS headers for main requests
+    headers = {
+        'Access-Control-Allow-Origin': '*',        
+    }
+
+    return ('Hello World!', 200, headers)
